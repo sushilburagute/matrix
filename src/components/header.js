9d753bbc042ff70aaef947c405a2392e08c05b43
Sushil Buragute <50674860+sushilburagute@users.noreply.github.com>
2020-07-04 17:25:03 +0530

    ✔️v1.0

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="title">
+        <span>JS Matrix</span>
+      </div>
+      <div className="subtitle">
+        <span>Blue Pill, Red Pill or the Green Pill? </span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
